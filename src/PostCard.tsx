417071e863a2f5fa2863313e9d1911fe2b90b6ae
sandra365 <postnsanna@gmail.com>
2024-07-17 20:53:00 +0200

    Style PostCard with MUI

--- src/PostCard.tsx
+++ src/PostCard.tsx
@@ -1,8 +1,10 @@
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import { Reactions } from './types';
 import { cropPostBody, formatTitle } from './utils';
 import './Post.css';
 import TagList from './TagList';
-import { Button } from '@mui/material';
+import { Button, List, Stack, Typography } from '@mui/material';
 
 interface PostProps {
     title: string;
@@ -17,15 +19,17 @@ function PostCard(props:PostProps) {
     
     return (
         <div className='post'>
-            <h2>{postTitle}</h2>
-            <ul><TagList tags={props.tags}/></ul>
-            <p>{postBodyPreview}</p>
+            <Typography variant='h5' component='h2'>{postTitle}</Typography>
+            <TagList tags={props.tags}/>
+            <Typography variant='body1'>{postBodyPreview}</Typography>
             <div className='post-bottom-container'>
                 <div className='post-reactions-container'>
                     <div>
+                        <ThumbUpIcon fontSize='small'/>
                         {props.reactions.likes}
                     </div>
                     <div>
+                        <ThumbDownIcon fontSize='small'/>
                         {props.reactions.dislikes}
                     </div>
                 </div>
--- src/TagList.tsx
+++ src/TagList.tsx
@@ -1,16 +1,35 @@
+import { Chip, List, ListItem, Stack } from "@mui/material";
+
 interface TagListProps {
     tags: string[];
 }
 
+// function TagList(props: TagListProps) {
+//     return props.tags.map((tag, index) => {
+//         const capitalizedTagName = tag[0].toUpperCase() + tag.slice(1);
+//         return (
+//             <ListItem key={index}>
+//                 <p>{capitalizedTagName}</p>
+//             </ListItem>
+//         );
+//     });
+// }
 function TagList(props: TagListProps) {
-    return props.tags.map((tag, index) => {
+    const itemList = props.tags.map((tag, index) => {
         const capitalizedTagName = tag[0].toUpperCase() + tag.slice(1);
         return (
-            <li key={index}>
-                <p>{capitalizedTagName}</p>
-            </li>
+                <Chip 
+                    key={index}
+                    component='li'
+                    label={capitalizedTagName}
+                />
         );
     });
+    return (
+        <Stack component='ul' direction='row' justifyContent='flex-start' spacing={1} sx={{ paddingLeft: 0 }}>
+            {itemList}
+        </Stack>
+    )
 }
 
 export default TagList;
