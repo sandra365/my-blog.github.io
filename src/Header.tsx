44b40667d1b8cb0b58c296c24d66263f038edf80
sandra365 <postnsanna@gmail.com>
2024-09-12 17:35:59 +0200

    improve header design

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,43 @@
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import MaterialLink from '@mui/material/Link';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <Typography variant='h1'> My blog </Typography>
+        <Stack
+            width='100%'
+            direction='row' 
+            alignItems='center' 
+            justifyContent='flex-start'
+            spacing={2}
+            position='sticky'
+            top={0}
+            bgcolor='#e3f2fd'
+            sx={{
+                zIndex: '100',
+                boxSizing: 'border-box',
+                '@media (max-width: 599px)': {
+                    paddingY: 1,
+                    paddingLeft: 2.5
+                },
+                '@media (min-width: 600px)': {
+                    paddingY: 2,
+                    paddingLeft: 5
+                }
+            }}
+        >
+            <Typography variant='h2' component='h1'> My blog </Typography>
+            <Link to='posts' style={{textDecoration: 'none'}}>
+                <MaterialLink variant='h5'component='h6' underline='hover' color='#000'>
+                    posts
+                </MaterialLink>
+            </Link>
+            <Link to='' style={{textDecoration: 'none'}}>
+                <MaterialLink variant='h5' component='h6' underline='hover' color='#000'>
+                    about
+                </MaterialLink>
+            </Link>
+        </Stack>
     );
 }
 
